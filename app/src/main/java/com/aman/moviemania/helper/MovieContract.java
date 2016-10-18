1bd9170744a0b500671bc8b108f3de483ebaa7a9
Aman Kapoor <kapoor.aman22@gmail.com>
2016-10-18 17:48:12 +0530

    Updated Contract with numeric and poster path

--- app/src/main/java/com/aman/moviemania/helper/MovieContract.java
+++ app/src/main/java/com/aman/moviemania/helper/MovieContract.java
@@ -10,7 +10,7 @@ public class MovieContract {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "movies.db";
     private static final String TEXT_TYPE = " TEXT";
-    private static final String INTEGER_TYPE = " INTEGER";
+    private static final String NUMBER_TYPE = " NUMBERIC";
     private static final String COMMA_SEP = ",";
 
     public MovieContract() {
@@ -23,14 +23,22 @@ public class MovieContract {
         public static final String COLUMN_RELEASE_DATE = "release_date";
         public static final String COLUMN_TITLE = "title";
         public static final String COLUMN_VOTE_AVERAGE = "vote_average";
+        public static final String COLUMN_POSTER_PATH = "poster_path";
+
+        public static final int COLUMN_INDEX_OVERVIEW = 3;
+        public static final int COLUMN_INDEX_RELEASE_DATE = 2;
+        public static final int COLUMN_INDEX_TITLE = 1;
+        public static final int COLUMN_INDEX_VOTE_AVERAGE = 4;
+        public static final int COLUMN_INDEX_POSTER_PATH = 5;
 
         public static final String CREATE_TABLE = "CREATE TABLE " +
                 TABLE_NAME + " (" +
                 _ID + " INTEGER PRIMARY KEY," +
-                COLUMN_OVERVIEW + TEXT_TYPE + COMMA_SEP +
-                COLUMN_RELEASE_DATE + TEXT_TYPE + COMMA_SEP +
                 COLUMN_TITLE + TEXT_TYPE + COMMA_SEP +
-                COLUMN_VOTE_AVERAGE + INTEGER_TYPE + " )";
+                COLUMN_RELEASE_DATE + TEXT_TYPE + COMMA_SEP +
+                COLUMN_OVERVIEW + TEXT_TYPE + COMMA_SEP +
+                COLUMN_VOTE_AVERAGE + NUMBER_TYPE + COMMA_SEP +
+                COLUMN_POSTER_PATH + TEXT_TYPE + " )";
         public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
     }
 
